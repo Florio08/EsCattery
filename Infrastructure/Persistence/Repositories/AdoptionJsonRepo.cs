6c3ebadeb0b348e669853a780ff9e9004f8f58e1
Francesco Florio <FRANCESCO.FLORIO@local.ittcesena.it>
2025-11-17 08:11:10 +0100

    inital commit

---
+++ Infrastructure/Persistence/Repositories/AdoptionJsonRepo.cs
@@ -0,0 +1,72 @@
+﻿using Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Model.Entities;
+using System.Text.Json;
+using Application.Dto;
+using Application.Mappers;
+namespace Infrastructure.Persistence.Repositories
+{
+    public class AdoptionJsonRepo : IAdoptionRepo
+    {
+        string filePath = "adoptions.json";
+        private List<Adoption> _cache = new List<Adoption>();
+        public void AddToRepo(Adoption itemToAdd)
+        {
+            EnsureLoaded();
+            _cache.Add(itemToAdd);
+            List<AdoptionDto> _dtovalues = new List<AdoptionDto>();
+            for(int i=0;i<_cache.Count;i++)
+            {
+                _dtovalues.Add(_cache[i].ToDto());
+            }
+            string json= JsonSerializer.Serialize(_dtovalues);
+            File.WriteAllText(filePath, json);
+        }
+
+        public IEnumerable<Adoption> GetAll()
+        {
+            EnsureLoaded();
+            return _cache;
+        }
+        private void EnsureLoaded()
+        {
+            if(_cache.Count > 0) return;//controllo molto superficiale, andrebbe visto se fosse in un serve se sono stati fatti altri accessi al file json
+            if (!File.Exists(filePath)) return;
+            string json = File.ReadAllText(filePath);
+            var dto = JsonSerializer.Deserialize<List<AdoptionDto>>(json);
+            for(int i=0;i<dto.Count;i++)
+            {
+                _cache.Add(dto[i].ToEntity());
+            }
+        }
+        public void RemoveFromRepo(Adoption itemToRemove)
+        {
+            EnsureLoaded();
+            _cache.Remove(itemToRemove);
+            List<AdoptionDto> _dtovalues = new List<AdoptionDto>();
+            for (int i = 0; i < _cache.Count; i++)//trafrormo la lista di domain in lista di dto e la serializzo
+            {
+                _dtovalues.Add(_cache[i].ToDto());
+            }
+            string json = JsonSerializer.Serialize(_dtovalues);//cosí rimuovo l'elemento dal json sovrascrivenbdolo con la nuova lista
+            File.WriteAllText(filePath, json);
+        }
+
+        public void Update(Adoption itemToUpdate)
+        {
+            EnsureLoaded();
+            for(int i=0;i<_cache.Count;i++)
+            {
+                if(_cache[i].Equals(itemToUpdate))
+                {
+                    _cache[i] = itemToUpdate;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/CatJsonRepo.cs
@@ -0,0 +1,86 @@
+﻿using Application.Dto;
+using Application.Interfaces;
+using Application.Mappers;
+using Domain.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class CatJsonRepo : ICatRepo
+    {
+        private List<Cat> _cache;
+        private string _filePath = "cats.json";
+        private void EnsureLoaded()
+        {
+            if (_cache.Count > 0) return;
+            if (!File.Exists(_filePath))
+            {
+                _cache = new List<Cat>();
+                return;
+            }
+            List<CatDto> dtoList = new List<CatDto>();
+            dtoList = JsonSerializer.Deserialize<List<CatDto>>(File.ReadAllText(_filePath));
+            for (int i = 0; i < dtoList.Count; i++)
+            {
+                _cache.Add(dtoList[i].ToEntity());
+            }
+        }
+
+        public Cat? GetByCui(string CUI)
+        {
+            EnsureLoaded();
+            for(int i = 0; i < _cache.Count; i++)
+            {
+                if (_cache[i].Cui == CUI) return _cache[i];
+            }
+            return null;
+        }
+        public IEnumerable<Cat> GetAll()
+        {
+            EnsureLoaded();
+            return _cache;
+        }
+        public void RemoveFromRepo(Cat itemToRemove)
+        {
+            EnsureLoaded();
+            _cache.Remove(itemToRemove);
+            List<CatDto> _dtovalues = new List<CatDto>();
+            for (int i = 0; i < _cache.Count; i++)//trafrormo la lista di domain in lista di dto e la serializzo
+            {
+                _dtovalues.Add(_cache[i].ToDto());
+            }
+            string jsonString = JsonSerializer.Serialize(_dtovalues);
+            File.WriteAllText(_filePath, jsonString);
+        }
+        public void AddToRepo(Cat itemToAdd)
+        {
+            EnsureLoaded();
+            _cache.Add(itemToAdd);
+            List<CatDto> _dtovalues = new List<CatDto>();
+            for (int i = 0; i < _cache.Count; i++)
+            {
+                _dtovalues.Add(_cache[i].ToDto());
+            }
+            string jsonString = JsonSerializer.Serialize(_dtovalues);
+            File.WriteAllText(_filePath, jsonString);
+        }
+        public void Update(Cat itemToUpdate)
+        {
+            EnsureLoaded();
+            for(int i = 0; i < _cache.Count; i++)
+            {
+                if(_cache[i].Equals(itemToUpdate))
+                {
+                    _cache[i] = itemToUpdate;
+                    break;
+                }
+            }
+
+        }
+    }
+}
